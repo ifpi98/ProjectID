8f97eb173690eb0f4e2e3a9f93bb9ac29a4ee2c4
YuhanKim <yuhankim@maverickgames.co>
2016-04-25 02:45:33 +0900

    Add : add all files

---
+++ Assets/2 - Scripts/Monster.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class Monster : MonoBehaviour
+{
+
+    public int hp;
+    public string text;
+    public bool monstersummon;
+    //public string[] unitData;
+    public string[,] unitData2 = new string[200,15];
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        hp = 50;
+        SetUnitData();
+
+    }
+
+
+    void SetUnitData()
+    {
+        UnitParser Unit = this.GetComponent<UnitParser>();
+        unitData2 = Unit._tempUD2;
+        //Debug.Log(unitData2[5,12]);
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    void OnGUI()
+    {
+        if (monstersummon == false)
+        {
+            GUI.Button(new Rect(10, 10, 200, 100), "HP : " + hp);
+        }
+
+        if (monstersummon == false)
+        {
+            GUI.Button(new Rect(10, 210, 200, 100), "Text : " + unitData2[5, 12]);
+        }
+//
+//        if (gameover == true && hp != 0)
+//        {
+//            if (GUI.Button(new Rect(10, 110, 200, 200), "Victory"))
+//           {
+//                Application.LoadLevel(Application.loadedLevel);
+//            }
+//        }
+    }
+
+}
---
+++ Assets/2 - Scripts/UnitParser.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class UnitParser : MonoBehaviour
+{
+    string path;
+    public string[] _unitData;
+    string[] _tempUD;
+    //public string[] _tempUD2;
+    public string[,] _tempUD2 = new string[200,15];
+
+    void Start()
+    {
+
+        path = "Assets/Resources/Datas/DataUnit.txt";
+        _unitData = System.IO.File.ReadAllLines(path);
+
+        if (_unitData.Length > 0)
+        {
+            for (int i = 0; i <_unitData.Length; i++)
+            {
+                //Debug.Log("Text : " + (i + 1).ToString() + "번째 줄.");
+                //Debug.Log(_unitData[i]);
+            }
+        }
+
+        Debug.Log(_unitData[_unitData.Length-1]);
+        //_tempUD2 = new string[_unitData.Length, 14];
+        Parse();
+
+        
+
+}
+
+    public void Parse()
+    {
+
+
+        for (int i = 0; i < _unitData.Length ; i++)
+        {
+  
+            string source = _unitData[i];
+            _tempUD = source.Split(',');
+
+            for (int y = 0; y < _tempUD.Length ; y++)
+            {
+                //Debug.Log(_tempUD.Length);
+                _tempUD2[i, y] = _tempUD[y];
+                //if (y == 13 && _tempUD[y] != "")
+                //{ Debug.Log(_tempUD[y]); }
+                
+            }
+
+        }
+
+        //Debug.Log(_tempUD2[5, 11]);
+        //Debug.Log(_tempUD2[5, 12]);
+        //Debug.Log(_tempUD2[5, 13]);
+    }
+    
+
+
+}
