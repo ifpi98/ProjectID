3286da8ef0d4bbe0007511919b4d5648ec3a911a
YuhanKim <yuhankim@maverickgames.co>
2016-04-26 03:38:58 +0900

    Add : add a GAME script

---
+++ Assets/2 - Scripts/Game.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+
+    public int[] cardSlot;
+    string cardSlot0;
+    string cardSlot1;
+    string cardSlot2;
+    string cardSlot3;
+    string cardSlot4;
+    int remainturncardslot0;
+    int remainturncardslot1;
+    int remainturncardslot2;
+    int remainturncardslot3;
+    int remainturncardslot4;
+    bool firstcheck = false;
+    bool secondcheck = false;
+    Monster mon;
+
+    // Use this for initialization
+    void Start ()
+    {
+        remainturncardslot0 = 3;
+        remainturncardslot1 = 3;
+        remainturncardslot2 = 3;
+        remainturncardslot3 = 3;
+        remainturncardslot4 = 3;
+        cardSlot = new int[5];
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (firstcheck == false)
+        {
+            mon = GameObject.Find("GameObj").GetComponent<Monster>();
+            firstcheck = true;
+        }
+
+        if (secondcheck == false)
+        {
+            int tempnum;
+            for (int i = 0; i < 5; i++)
+            {
+                tempnum = Random.RandomRange(0, mon.charcount);
+                cardSlot[i] = tempnum;
+            }
+            cardSlot0 = mon.charData2[cardSlot[0], 1];
+            cardSlot1 = mon.charData2[cardSlot[1], 1];
+            cardSlot2 = mon.charData2[cardSlot[2], 1];
+            cardSlot3 = mon.charData2[cardSlot[3], 1];
+            cardSlot4 = mon.charData2[cardSlot[4], 1];
+            //cardSlot1 = Random.RandomRange(0, mon.charcount);
+            //cardSlot2 = Random.RandomRange(0, mon.charcount);
+            //cardSlot3 = Random.RandomRange(0, mon.charcount);
+            //cardSlot4 = Random.RandomRange(0, mon.charcount);
+            secondcheck = true;
+        }
+        
+
+    }
+
+    void OnGUI()
+    {
+        GUI.Button(new Rect(10, 10, 150, 100), cardSlot0 );
+        GUI.Button(new Rect(160, 10, 150, 100),cardSlot1);
+        GUI.Button(new Rect(310, 10, 150, 100),cardSlot2);
+        GUI.Button(new Rect(460, 10, 150, 100),cardSlot3);
+        GUI.Button(new Rect(610, 10, 150, 100),cardSlot4);        
+        GUI.Button(new Rect(10, 120, 150, 50), "Remain Turn : " + remainturncardslot0);
+        GUI.Button(new Rect(160, 120, 150, 50), "Remain Turn : " + remainturncardslot1);
+        GUI.Button(new Rect(310, 120, 150, 50), "Remain Turn : " + remainturncardslot2);
+        GUI.Button(new Rect(460, 120, 150, 50), "Remain Turn : " + remainturncardslot3);
+        GUI.Button(new Rect(610, 120, 150, 50), "Remain Turn : " + remainturncardslot4);
+
+        //
+        //        if (gameover == true && hp != 0)
+        //        {
+        //            if (GUI.Button(new Rect(10, 110, 200, 200), "Victory"))
+        //           {
+        //                Application.LoadLevel(Application.loadedLevel);
+        //            }
+        //        }
+    }
+}
--- Assets/2 - Scripts/Monster.cs
+++ Assets/2 - Scripts/Monster.cs
@@ -5,19 +5,18 @@ using System.IO;
 public class Monster : MonoBehaviour
 {
 
-    public int hp;
-    public string text;
-    public bool monstersummon;
     //public string[] unitData;
     public string[,] unitData2 = new string[200,15];
     public string[,] charData2 = new string[200, 15];
     public string[,] locData2 = new string[400, 15];
+    public int charcount;
+    public int unitcount;
+    public int loccount;
 
     // Use this for initialization
     void Start()
     {
 
-        hp = 50;
         SetCharData();
         SetUnitData();
         SetLocData();
@@ -29,7 +28,7 @@ public class Monster : MonoBehaviour
     {
         CharParser Char1 = this.GetComponent<CharParser>();
         charData2 = Char1._tempCD2;
-        int charcount = Char1._charData.Length;
+        charcount = Char1._charData.Length;
         //Debug.Log(charData2[1, 1]);
         //Debug.Log(charcount);
     }
@@ -38,7 +37,7 @@ public class Monster : MonoBehaviour
     {
         UnitParser Unit1 = this.GetComponent<UnitParser>();
         unitData2 = Unit1._tempUD2;
-        int unitcount = Unit1._unitData.Length;
+        unitcount = Unit1._unitData.Length;
         //Debug.Log(unitData2[1,1]);
         //Debug.Log(unitcount);
     }
@@ -47,7 +46,7 @@ public class Monster : MonoBehaviour
     {
         LocalizationParser Loc1 = this.GetComponent<LocalizationParser>();
         locData2 = Loc1._tempLD2;
-        int loccount = Loc1._localizationData.Length;
+        loccount = Loc1._localizationData.Length;
         //Debug.Log(locData2[1, 1]);
         //Debug.Log(loccount);
     }
@@ -59,25 +58,6 @@ public class Monster : MonoBehaviour
        
     }
 
-    void OnGUI()
-    {
-        if (monstersummon == false)
-        {
-            GUI.Button(new Rect(10, 10, 200, 100), "HP : " + hp);
-        }
-
-        if (monstersummon == false)
-        {
-            GUI.Button(new Rect(10, 210, 200, 100), "Text : " + unitData2[5, 12]);
-        }
-//
-//        if (gameover == true && hp != 0)
-//        {
-//            if (GUI.Button(new Rect(10, 110, 200, 200), "Victory"))
-//           {
-//                Application.LoadLevel(Application.loadedLevel);
-//            }
-//        }
-    }
+    
 
 }
