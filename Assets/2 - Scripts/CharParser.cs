2b055cd08ff0eaeb207ceb63e67c0647eba5b401
YuhanKim <yuhankim@maverickgames.co>
2016-05-08 04:09:52 +0900

    Mod : Updata char and unit data

--- Assets/2 - Scripts/CharParser.cs
+++ Assets/2 - Scripts/CharParser.cs
@@ -37,7 +37,7 @@ public class CharParser : MonoBehaviour
     public void Parse()
     {
         
-        _charData = new string[69];
+        _charData = new string[72];
 
   
         for (int i = 0; i < sourceChar.Length; i++)
